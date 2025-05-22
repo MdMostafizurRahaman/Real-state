399c304258e07376af8264594f94f798c2c623ef
Rakib <bsse1320@iit.du.ac.bd>
2025-05-22 14:59:12 +0600

    db connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log('MongoDB connected!!!!!......')
+    }).catch((err) => {
+        console.log(err)
+    })
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("Server Conected!!!!........")
 })
+
