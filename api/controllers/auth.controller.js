82acd10c4516504e363aceecf379f9ac5418d8e4
Rakib <bsse1320@iit.du.ac.bd>
2025-05-26 08:47:39 +0600

    complete signUp page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
-import { errorHandler } from "../utlis/error.js"
 
 export const signup = async(req, res, next) => {
     const {username, email, password} = req.body
@@ -10,6 +9,6 @@ export const signup = async(req, res, next) => {
         await newUser.save()
         res.status(201).json("User created successfully")
     } catch (error) {
-        next(errorHandler(550, 'Error from function'))
+        next(error)
     }
 }
